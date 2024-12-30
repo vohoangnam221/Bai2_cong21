834029b143f0fb72348c2705a6694c38392052ef
SPC <SPC@17-LAB402>
2024-12-30 09:52:43 +0700

    Add project files.

---
+++ Bai2_cong2.cpp
@@ -0,0 +1,27 @@
+// Bai2_cong2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "CHUONG TRINH TINH TONG HAI SO\n";
+int so1, so2;
+int tong;
+so1 = 3;
+so2 = 9;
+tong = so1 + so2;
+cout << "tong hai so: " << so1 << " + " << so2 << " = " << tong << endl;
+return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
